603bad5d3c5a2a16b7ac38734bf196e02fdc9475
Adrian Hayes <178721588+panfriedpixels@users.noreply.github.com>
2024-08-31 12:13:32 +0100

    Islands and light/dark mode

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+import preact from '@astrojs/preact';
 
 // https://astro.build/config
 export default defineConfig({
   // devToolbar: { enabled: false },
+  site: 'https://astro-adie.netlify.app',
+  integrations: [preact()],
 });
